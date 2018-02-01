eb6a7dadfc8221c6d9ca3028dc38aab5633ed884
Jan Joneš <jjones@outlook.cz>
2018-02-01 19:39:31 +0100

    values backup

--- src/lisp_interpreter/environment.hpp
+++ src/lisp_interpreter/environment.hpp
@@ -11,7 +11,7 @@ class environment {
 public:
     environment(const environment& parent) : parent(parent) {}
     const environment& parent;
-    std::map<std::string, cep> map;
+    std::map<std::string, ep> map; // TODO: map field should be read-only
 };
 
 #endif
--- src/lisp_interpreter/values.hpp
+++ src/lisp_interpreter/values.hpp
@@ -4,7 +4,11 @@
 #ifndef _H_VALUES
 #define _H_VALUES
 
+#include <memory>
+
 class visitor;
+class e_pair;
+class environment;
 
 class value {
 public:
@@ -12,10 +16,31 @@ public:
     virtual void accept(visitor& v) const = 0;
 };
 
+using vp = std::shared_ptr<value>;
+using cvp = const vp;
+
 class internal_value : public value {
 public:
     virtual bool is_primitive() { return false; }
     virtual bool is_lambda() { return false; }
+    virtual cvp eval(const e_pair& args) { throw std::runtime_error("not a function"); }
+};
+
+using prim_func = cvp(*)(const e_pair&);
+
+class v_primitive : public internal_value {
+public:
+    v_primitive(prim_func func) : func_(func) {}
+    bool is_primitive() override { return true; }
+    cvp eval(const e_pair& args) override { return func_(args); }
+private:
+    prim_func func_;
+};
+
+class v_lambda : public internal_value {
+private:
+    const e_pair& val_;
+    const environment& env_;
 };
 
 #endif
